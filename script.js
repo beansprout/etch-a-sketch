d7a5f42986a86c8feaba1c553b9a87a0c721658c
Christine Gierer <beansprout@users.noreply.github.com>
2019-03-24 21:39:06 -0400

    added reset button and toggle shape of grid button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //-------- define colors-----------
-const bgColor = '#3bb4c1'; // grid box color
-const color1 = '#3bb4c1'; // h1 color
-const color2 = '#b5e0ba'; // button background
-const color3 = '#e9e4e6'; // button shadow
+const bgColor = '#cdffeb'; // grid box color
+const color1 = '#ffaaaa'; // h1 color
+const color2 = '#c7004c'; // button background
+const color3 = '#8f1537'; // button shadow
 const color4 = '#f6f5f5';
 const color5 = '#33313b';
+const color6 = '#cdffeb'; //button font
 
 const varyColor = () => {
   let color = Math.floor((Math.random()*1e5));
@@ -30,16 +31,24 @@ bleep.src = 'lesser_vibes_HTIS_Buttons_59_013.mp3';
 // --------misc -------------------
 document.querySelector('h1').style.color = color1;
 document.querySelector('body').style.backgroundColor = color5;
-const numOfButtons = 3;
+const numOfButtons = 4;
+
 
 //--------- containers -------------
 const container = document.getElementById('container');
 const rightContainer = document.getElementById('right-container');
+const instructions = document.createElement('p');
+rightContainer.appendChild(instructions);
+instructions.innerHTML = 'Instructions: Move the mouse around on the grid';
+instructions.style.fontSize = '24px'
+instructions.style.color = color1;
+instructions.marginTop = '-30px';
+
 // define container as grid
 container.style.display = 'grid';
 container.style.width = '1060px';
 // set column and row sizes
-let gridSize = 4 // grid size example 2 equals 2x2 grid
+let gridSize = 16 // grid size example 2 equals 2x2 grid
 
 // ---------- hover effect function-------------
 const hoverEffect = () => {
@@ -54,9 +63,10 @@ const hoverEffect = () => {
 
 hoverEffect();
 
+
 const createGrid = (gridSize) => {
-  const colSize = (1060 / gridSize) + 'px'; //100% divided by x divs
-  const rowSize = (960 / gridSize) + 'px';
+  const colSize = (960 / gridSize) + 'px'; //100% divided by x divs
+  const rowSize = (860 / gridSize) + 'px';
   container.style.gridTemplateColumns = `repeat(${gridSize}, ${colSize})`;
   container.style.gridTemplateRows = `repeat(${gridSize}, ${rowSize})`;
   container.style.gridColumnGap = '2px';
@@ -77,17 +87,17 @@ const createGrid = (gridSize) => {
 createGrid(gridSize);
 
 //  -------------- change/ set pen color ------
-  let setColor = varyColor();
+  let setColor = color1;
 
 
 // ------remove grid---------------------------
 const removeGrid = () => {
-  console.log(container);
   while (container.firstElementChild) {
     container.removeChild(container.firstElementChild);
   }
 
 }
+
 // ------ buttons -----------------------------
 
 const createButtons = (num) => {
@@ -95,22 +105,22 @@ const createButtons = (num) => {
   let p;
   for (let i = 0; i < num; i++) {
     btn = document.createElement('div');
-    p = document.createElement('p');
+    // p = document.createElement('p');
     rightContainer.appendChild(btn);
-    rightContainer.appendChild(p);
+    // rightContainer.appendChild(p);
     btn.className = 'btn';
     btn.id ='btn'+ i;
     btn.style.width = '160px;'
-    btn.style.height = '100px;'
+    btn.style.height = '70px;'
     btn.innerHTML = 'I\'m  a button';
     btn.style.padding = '20px';
     btn.style.background = color2;
     btn.style.borderRadius = '25px';
-    btn.style.fontSize = '40px';
-    btn.style.margin = "0 0 100px";
+    btn.style.fontSize = '28px';
+    btn.style.color = color6;
+    btn.style.margin = "0 0 30px";
     btn.style.cursor = 'pointer';
-    btn.style.transition = '0.2s';
-    btn.style.boxShadow = `${color3} 1px 1px 10px 10px`;
+    btn.style.transition = '0.1s';
   }
 }
 
@@ -118,45 +128,123 @@ createButtons(numOfButtons);
 
 const btn0 = document.getElementById('btn0');
 const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
 const btns = document.querySelectorAll('.btn')
+
+const text1 = document.createElement('p');
+const text2 = document.createElement('p');
+text1.innerHTML = 'To choose pen color<br>click the box below';
+text2.innerHTML = '';
+text1.style.color = color2;
+text2.style.color = color2;
+text1.style.fontSize = '28px';
+text2.style.fontSize = '28px'
+rightContainer.appendChild(text1);
+rightContainer.appendChild(text2);
+// color selector -----------------------------
+const colorSelector = document.createElement('input');
+colorSelector.type = 'color';
+colorSelector.id = 'colorSelect';
+colorSelector.value = setColor;
+colorSelector.style.cursor = 'pointer';
+rightContainer.appendChild(colorSelector);
+colorSelector.style.transform = 'scale(2.0)'
+colorSelector.addEventListener('mouseenter', (e) => {
+  colorSelector.style.transform = ('scale(2.2)');
+  colorSelector.style.boxShadow = (`${color4} 1px 1px 50px`)
+});
+colorSelector.addEventListener('mouseout', (e) => {
+  colorSelector.style.transform = ('scale(2.0)');
+  colorSelector.style.boxShadow = (``)
+});
+
 
 const setButtonEffects = (num) => {
   for (let i = 0; i < num; i++) {
     btns[i].addEventListener('mousedown', (e) => {
       btns[i].style.background = color1;
-      console.log(e.target)
     })
     btns[i].addEventListener('mouseup', (e) => {
       btns[i].style.background = color2;
     })
     btns[i].addEventListener('mouseenter', (e) => {
       btns[i].style.transform = ('scale(1.2)');
-      btns[i].style.boxShadow = (`${color4} 2px 2px 100px`);
+      btns[i].style.boxShadow = (`${color4} 2px 2px 50px`);
     })
     btns[i].addEventListener('mouseleave', (e) => {
       btns[i].style.transform = ('scale(1.0)');
-      btns[i].style.boxShadow = (`${color3} 1px 1px 10px 10px`)
+      btns[i].style.boxShadow = (``)
     })
   }
 }
 
 setButtonEffects(numOfButtons);
 
+const gridSizePrompt = () => {
+  let newGridSize = prompt('New grid size: How many units per side?');
+  console.log(newGridSize);
+  const doIt = () => {
+    removeGrid();
+    createGrid(newGridSize);
+    const boxes = document.querySelectorAll('.box');
+  }
+  if (newGridSize <= 0 || newGridSize.match(/[^0-9]/g) !== null) {
+    alert('Nice try butterfly but that is an invalid input.  Try again.')
+    gridSizePrompt()
+  }
+  if (newGridSize > 0 && newGridSize < 25) {
+    doIt();
+  }
+  if (newGridSize >= 25) {
+    alert('Please wait... This may take a while....');
+    doIt();
+  }
+  if (newGridSize >= 40) {
+    alert('Sorry - that grid is too big for my little processor to handle.  Please try again')
+    gridSizePrompt();
+  }
+};
+
 btn0.innerHTML = 'Click to change grid size';
 btn0.addEventListener('click', (e) => {
-  let newGridSize = prompt('New grid size: How many units per side?');
+  gridSizePrompt();
+});
+
+btn1.innerHTML = 'Click to reset';
+btn1.addEventListener('click', (e) => {
   removeGrid();
-  createGrid(newGridSize);
+  createGrid(16);
+  // const boxes = document.querySelectorAll('.box');
+});
+
+btn2.innerHTML = 'Click to toggle shape of grid units';
+btn2.addEventListener('click', (e) => {
   const boxes = document.querySelectorAll('.box');
+  let newBorderRadius;
+  console.log(boxes[0].style.borderRadius);
+  if (boxes[0].style.borderRadius === '100%') {
+    newBorderRadius = '';
+  } else {
+    newBorderRadius = '100%';
+  }
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.borderRadius = newBorderRadius;
+  }
 })
 
-btn1.innerHTML = 'Click to change to random pen color';
-btn1.addEventListener('click', (e) => {
+btn3.innerHTML = 'Click to change to random pen color';
+btn3.addEventListener('click', (e) => {
   setColor = varyColor();
-  console.log(setColor);
   bleep.play();
-})
+});
+
+
+
+colorSelector.addEventListener('change', (e) => {
+  console.log(colorSelector.value);
+  setColor = colorSelector.value;
+});
+
 
 
 
